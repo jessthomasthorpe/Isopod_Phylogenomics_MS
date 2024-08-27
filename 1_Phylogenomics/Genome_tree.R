3bcba41b3f3ff47ba2d752c00d721dcd5e9ecd09
jessthomasthorpe <83020435+jessthomasthorpe@users.noreply.github.com>
2024-08-27 16:48:30 +0100

    Update Genome_tree.R
    updated for new data

--- 1_Phylogenomics/Genome_tree.R
+++ 1_Phylogenomics/Genome_tree.R
@@ -21,8 +21,7 @@ my_colors <- unique(meta$Colour[order(meta$Group)])
 #print(my_colors)
 my_colors_21 <- c('#2D2D2D',my_colors) #switch these around for dS or if colours in wrong order
 #print(my_colors_21)
-t2 <-ggtree(IsopodatreeGenome, aes(color=group))+scale_colour_manual(guide = FALSE, values = my_colors_21)  
-#t2
-t2 + geom_tiplab(as_ylab=FALSE, size=5, color='black', fontface = "italic", hjust=-.1) + geom_text2(aes(subset = !isTip,label=label),size=2, color='black', hjust=-.1) + ggplot2::xlim(0, 1.5)
-#nodelabels(text = IsopodatreeGenome$node.label, frame = "n", cex=0.8, col= "black")
+t2 <-ggtree(IsopodatreeGenome, aes(color=group))+scale_colour_manual(values = my_colors_21)  
+t2 + geom_tiplab(as_ylab=FALSE, size=3, color='black', fontface = "italic", hjust=-.1) + geom_text2(aes(subset = !isTip,label=label),size=2, color='black', hjust=-.1) + ggplot2::xlim(0, 1.5) + theme(legend.position='none')
+
 
